b69f8da203724eba90cf0e8e8fa12542be7fc572
BenBarnes <barnes.benjamin@gmail.com>
2026-02-08 10:31:17 +0100

    Initial Commit

---
+++ HIITTimerApp/HIITTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITTimerAppApp.swift
+//  HIITTimerApp
+//
+//  Created by Benjamin Barnes on 08.02.26.
+//
+
+import SwiftUI
+
+@main
+struct HIITTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HIITTimerAppTests/HIITTimerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HIITTimerAppTests.swift
+//  HIITTimerAppTests
+//
+//  Created by Benjamin Barnes on 08.02.26.
+//
+
+import Testing
+@testable import HIITTimerApp
+
+struct HIITTimerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
